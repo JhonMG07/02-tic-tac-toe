587f8b26148c6b48cf037940045e7c97e316a301
jhon-meza <jhon.meza@ioet.com>
2023-07-07 11:08:28 -0500

    feat(Add new function): :sparkles: Now you cant rewrite de turn

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,64 @@ const TURNS = {
 
 const Squere = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  const handleClick =()=>{
-    updateBoard();
-  }
+  const handleClick = () => {
+    updateBoard(index);
+  };
   return (
     <div onClick={handleClick} className={className}>
       {children}
     </div>
   );
 };
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
-
-  //estado para saber de quien es el turno
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); // null= no hay ganaador y false = hay empate
 
-  const updateBoard = () => {
-    const newTurn = turn === TURNS.X? TURNS.O: TURNS.X;
+  const checkWinner = (boardToCheck) =>{
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo;
 
+      
+    }
+
+  }
+  
+
+
+  const updateBoard = (index) => {
+    //Si ya hay algo en esa posicion no actualizar
+    if (board[index]) return;
+
+    //los datos siempre deben ser nuevos (principio de inmutabilidad. Usar siempre set para modificar un estado)
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
   };
+
   return (
     <main className="board">
       <h1>tic tac toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Squere key={index} index={index} updateBoard={updateBoard}> 
-            {/*Tener en cuenta el momento de pasar la funcion o la ejecucion de la funcion */}
+            <Squere key={index} index={index} updateBoard={updateBoard}>
+              {/*Tener en cuenta el momento de pasar la funcion o la ejecucion de la funcion */}
               {board[index]}
             </Squere>
           );
